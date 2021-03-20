96b62deff513ccf6dd8a975effef6475bf4b2fcd
John-Holden <johnholden7@gmail.com>
2021-03-20 19:15:01 +0000

    full dataset added

--- main.py
+++ main.py
@@ -29,8 +29,6 @@ if __name__ == '__main__':
     from landscape_control.plotting_methods import cluster_sizes_vs_beta
     from landscape_control.domain_processing import get_clusters_over_betas
 
-    ens = EnsembleInfo('landscape_control_package_upper_lower_bounds')
+    ens = EnsembleInfo('landscape_control_package_adb_full')
     cluster_sizes = get_clusters_over_betas(ens, plot_clusters=True)
-
     # get_clusters_over_betas(ensemble, plot_output=True, plot_clusters=True)
-
